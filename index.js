6feb8b429b7d1eb3a3dbff54a57c806f632efe5f
chemingjun <chemingjun@weidian.com>
2016-11-09 13:06:40 +0800

    upgrade

--- index.js
+++ index.js
@@ -8,34 +8,16 @@
  * Author: CheMingjun <chemingjun@126.com>
  */
 'use strict';
-var assert = require('assert').ok,anno = require('./lib/anno');
+var anno = require('./lib/anno');
 //---------------------------------------------------------------------------------------------
 var Rtn = {};
 module.exports = Rtn;
 /**
- * The configuaration of Toybricks
- * @param _cfg {logger}
- */
-Rtn.config=function (_cfg) {
-    var util = require("./lib/util")
-    if (typeof _cfg === 'object') {
-        if (typeof _cfg.logger === 'function') {
-            util.init(_cfg.logger);
-        }
-        if (util.is.array(_cfg.annotations)) {
-            _cfg.annotations.forEach(anno.reg);
-        }
-    }
-};
-/**
- * register new annotatioin
+ * define a  annotatioin
  * @param _name
  * @param _impl
  */
-Rtn.regAnnotation = anno.reg;
-//---------------------------------------------------------------------------
-Rtn.regAnnotation('logger',{
-    scope: 'var', build: function () {
-        return "return require('toybricks/lib/util').log();\n"
-    }
-})
+Rtn.define = function () {
+    anno.def.apply(this, Array.prototype.slice.call(arguments));
+    return Rtn;
+};
